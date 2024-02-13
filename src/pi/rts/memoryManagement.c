deeace29a52ee5cb93810d8a693b45c677a57af5
Rafael <rafael.prevost.15@gmail.com>
2024-02-13 09:49:24 -0500

    Remove condition in idris2_refc_verify_failed

--- src/pi/rts/memoryManagement.c
+++ src/pi/rts/memoryManagement.c
@@ -14,7 +14,8 @@ Value *newValue(size_t size) {
 }
 
 void init () {
-  IDRIS2_REFC_VERIFY(NULL, "malloc failed");
+  char *test = (char *) malloc(15);
+  IDRIS2_REFC_VERIFY(test, "malloc failed");
  }
 
 
--- src/pi/rts/refc_util.h
+++ src/pi/rts/refc_util.h
@@ -7,16 +7,15 @@
 #define IDRIS2_REFC_VERIFY(cond, ...)                                          \
   do {                                                                         \
     if (!(cond)) {                                                             \
-      idris2_refc_verify_failed(__FILE__, __LINE__);       \
+      idris2_refc_verify_failed(__FILE__, __LINE__, #cond, __VA_ARGS__);       \
     }                                                                          \
   } while (0)
 
 // Used by `IDRIS2_REFC_VERIFY`, do not use directly.
-void idris2_refc_verify_failed(const char *ptr, const char *line) {
-  if(ptr == NULL) {
-    print(line);
-    exit();
-  }
+void idris2_refc_verify_failed(const char *ptr, const char *line,  const char *cond,
+                               const char *fmt, ...) {
+  print(fmt);
+  exit();
 }
 
 
